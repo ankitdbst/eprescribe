56b7076880eee4a159da2b902cb0006ac5e7e04f
Mohanish Singh <mohanish.singh@gmail.com>
2016-01-19 19:38:35 +0530

    Corrected Menu issues

--- app/Login/login.js
+++ app/Login/login.js
@@ -1,17 +1,34 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular.module('ERemediumWebApp.login', ['ui.router', 'ngMessages'])
+    angular.module('ERemediumWebApp.login', ['ui.router', 'ngMessages'])
+            .config(['$stateProvider', function ($stateProvider) {
+                    $stateProvider
+                            .state('login', {
+                                url: '/login',
+                                templateUrl: 'Login/login.html',
+                                controller: 'LoginCtrl'
+                            });
+                }])
+            .controller('LoginCtrl', LoginCtrl);
 
-.
-config(['$stateProvider', function($stateProvider) {
-  $stateProvider
-  .state('login', {
-    url: '/login',
-    templateUrl: 'Login/login.html',
-    controller: 'LoginCtrl'
-  });
-}])
+    LoginCtrl.$inject = ['$scope', '$rootScope', '$state'];
 
-.controller('LoginCtrl', [function() {
+    function LoginCtrl($scope, $rootScope, $state) {
+        $rootScope.showMenu = false;
+        
+        $scope.signIn = SignIn;
 
-}]);
+        function SignIn() {
+            //validate using username and password
+            alert('You are awesome! Signing In...');
+            
+            //start showing menu items 
+            $rootScope.showMenu = true;
+            
+            //Navigate to First Page in menu
+            $state.go('PatientsList');
+        }
+    }
+
+})();
--- app/js/app.js
+++ app/js/app.js
@@ -2,18 +2,23 @@
 
 // Declare app level module which depends on views, and components
 angular.module('ERemediumWebApp', [
-  'ui.router',
-  'ui.bootstrap',
-  'ngResource',
-  'ngMessages',
-  'ERemediumWebApp.patients',
-  'ERemediumWebApp.reportinganalytics',
-  'ERemediumWebApp.doctor_settings',
-  'ERemediumWebApp.login',
-  'ERemediumWebApp.prescriptions',
-  'ERemediumWebApp.utils'
+    'ui.router',
+    'ui.bootstrap',
+    'ngResource',
+    'ngMessages',
+    'ERemediumWebApp.patients',
+    'ERemediumWebApp.reportinganalytics',
+    'ERemediumWebApp.doctor_settings',
+    'ERemediumWebApp.login',
+    'ERemediumWebApp.prescriptions',
+    'ERemediumWebApp.utils'
 ])
 
-.config(['$urlRouterProvider', function($urlRouterProvider) {
-  $urlRouterProvider.otherwise('/login');
-}]);
+        .config(['$urlRouterProvider', function ($urlRouterProvider) {
+                $urlRouterProvider.otherwise('/login');
+            }]);
+
+$(".sidebar-nav a").on("click", function () {
+    $(".sidebar-nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+});
