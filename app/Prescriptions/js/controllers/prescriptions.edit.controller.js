85f0daae028a39b6213617b469f62077fdb41add
Mohanish Singh <mohanish.singh@gmail.com>
2016-01-21 20:31:06 +0530

    Added Header in Edit Prescription Page

--- app/Prescriptions/js/controllers/prescriptions.edit.controller.js
+++ app/Prescriptions/js/controllers/prescriptions.edit.controller.js
@@ -1,62 +1,64 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular.module('ERemediumWebApp.prescriptions.controllers')
-  .controller('PrescriptionNewOrEditCtrl', PrescriptionNewOrEditCtrl);
+    angular.module('ERemediumWebApp.prescriptions.controllers')
+            .controller('PrescriptionNewOrEditCtrl', PrescriptionNewOrEditCtrl);
 
-  PrescriptionNewOrEditCtrl.$inject = ['$scope', 'Prescription', '$stateParams', '$state'];
+    PrescriptionNewOrEditCtrl.$inject = ['$scope', 'Prescription', '$stateParams', '$state', '$rootScope'];
 
-  function PrescriptionNewOrEditCtrl($scope, Prescription, $stateParams, $state) {
-    var pid = $stateParams.id;
+    function PrescriptionNewOrEditCtrl($scope, Prescription, $stateParams, $state, $rootScope) {
+        //Intialize
+        $rootScope.pageHeader = "Prescription Detail";
+        var pid = $stateParams.id;
 
-    if (pid !== undefined && pid.length !== 0) {
-      $scope.prescription = Prescription.get({
-        user: 'sujeet',
-        sessionId: '78131321',
-        pid: pid
-      });
-      $scope.prescription.isUpdate = true; // for edit we change this to true
-    } else {
-      $scope.prescription = new Prescription();
-      // Fill defaults from session object maybe
-      $scope.prescription.isUpdate = false; // for edit we change this to true
-      // Medications
-      $scope.prescription.medcines = [];
+        if (pid !== undefined && pid.length !== 0) {
+            $scope.prescription = Prescription.get({
+                user: 'sujeet',
+                sessionId: '78131321',
+                pid: pid
+            });
+            $scope.prescription.isUpdate = true; // for edit we change this to true
+        } else {
+            $scope.prescription = new Prescription();
+            // Fill defaults from session object maybe
+            $scope.prescription.isUpdate = false; // for edit we change this to true
+            // Medications
+            $scope.prescription.medcines = [];
+        }
+
+        $scope.saved = false;
+
+        // Methods
+        $scope.save = UpsertPrescription;
+        $scope.close = Close;
+        $scope.order = Order;
+
+        function UpsertPrescription() {
+            $scope.prescription.patientId = 2;
+            $scope.prescription.doctorId = 101;
+
+            var params = {
+                user: 'sujeet',
+                sessionId: '78131321',
+                prescription: $scope.prescription
+            };
+            Prescription.upsert(params, $scope.prescription);
+            $scope.saved = true;
+        }
+
+        function Close() {
+            $state.go('PrescriptionList');
+        }
+
+        function Order() {
+            if (!$scope.saved) { // Defensive
+                UpsertPrescription();
+            }
+
+            $state.go('PrescriptionOrder', {
+                id: $scope.prescription.pid
+            });
+        }
     }
 
-    $scope.saved = false;
-
-    // Methods
-    $scope.save = UpsertPrescription;
-    $scope.close = Close;
-    $scope.order = Order;
-
-    function UpsertPrescription() {
-      $scope.prescription.patientId = 2;
-      $scope.prescription.doctorId = 101;
-
-      var params = {
-        user: 'sujeet',
-        sessionId: '78131321',
-        prescription: $scope.prescription
-      };
-      Prescription.upsert(params, $scope.prescription);
-      $scope.saved = true;
-    }
-
-    function Close() {
-      $state.go('PrescriptionList');
-    }
-
-    function Order() {
-      if( !$scope.saved ) { // Defensive
-        UpsertPrescription();
-      }
-
-      $state.go('PrescriptionOrder', {
-        id: $scope.prescription.pid
-      });
-    }
-  }
-
-}) ();
+})();
