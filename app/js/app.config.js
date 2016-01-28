4e1031c3e800c8a744f4c8eacd4ee51eac0fe72f
Ankit Gupta <ankigup@adobe.com>
2016-01-28 17:45:57 +0530

    Add transformRequest function for removing fields starting with $ and _id field for mongo to upsert properly.

---
+++ app/js/app.config.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  angular.module('ERemediumWebApp.config')
+
+  .config([
+    '$httpProvider',
+
+    function($httpProvider) {
+      function transformRequest(data) {
+        if (data == undefined) {
+          return data;
+        }
+
+        angular.forEach(data, function(value, key) {
+          if (key.startsWith('$') || key == '_id') { // Backend service fails if we have these params in the request
+            delete data[key];
+          }
+        });
+        return data;
+      }
+
+      $httpProvider.defaults.transformRequest.unshift(transformRequest);
+    }
+  ]);
+}) ();
--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,29 @@
-'use strict';
+(function () {
+  'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('ERemediumWebApp', [
-    'ui.router',
-    'ui.bootstrap',
-    'ngResource',
-    'ngMessages',
-    'ERemediumWebApp.patients',
-    'ERemediumWebApp.reportinganalytics',
-    'ERemediumWebApp.doctor_settings',
-    'ERemediumWebApp.login',
-    'ERemediumWebApp.prescriptions',
-    'ERemediumWebApp.utils'
-])
+  // Declare app level module which depends on views, and components
+  angular.module('ERemediumWebApp', [
+      'ui.router',
+      'ui.bootstrap',
+      'ngResource',
+      'ngMessages',
+      'ERemediumWebApp.config',
+      'ERemediumWebApp.routes',
+      'ERemediumWebApp.patients',
+      'ERemediumWebApp.reportinganalytics',
+      'ERemediumWebApp.doctor_settings',
+      'ERemediumWebApp.login',
+      'ERemediumWebApp.prescriptions',
+      'ERemediumWebApp.utils'
+  ]);
 
-//.config(['$urlRouterProvider', function ($urlRouterProvider) {
-//        $urlRouterProvider.otherwise('/login');
-//    }]);
+  angular.module('ERemediumWebApp.routes', []);
+  angular.module('ERemediumWebApp.config', []);
 
-//For correctly applying Active Class on Side Menu
-$(".sidebar-nav a").on("click", function () {
-    $(".sidebar-nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-});
+  // TODO: Move this to directive
+  //For correctly applying Active Class on Side Menu
+  $(".sidebar-nav a").on("click", function () {
+      $(".sidebar-nav").find(".active").removeClass("active");
+      $(this).parent().addClass("active");
+  });
+}) ();
