c231ca842c0208c7fa5098d4a9c2a580e7fe73fb
Ankit Gupta <ankigup@adobe.com>
2016-03-07 10:17:11 +0530

    Make canvas editor full screen for Prescriptions.

--- app/Utils/js/directives/my-canvas.directive.js
+++ app/Utils/js/directives/my-canvas.directive.js
@@ -9,6 +9,28 @@
     function myCanvas($rootScope) {
         function link(scope, element, attrs) {
             var canvas = element.get(0);
+
+            if( attrs.fullScreen )
+              canvas.height = window.innerHeight;
+
+            // Adjust canvas coordinate space taking into account pixel ratio,
+            // to make it look crisp on mobile devices.
+            // This also causes canvas to be cleared.
+            function resizeCanvas() {
+                // When zoomed out to less than 100%, for some very strange reason,
+                // some browsers report devicePixelRatio as less than 1
+                // and only part of the canvas is cleared then.
+                var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+                canvas.width = canvas.offsetWidth * ratio;
+                canvas.height = canvas.offsetHeight * ratio;
+                console.log("Ratio: ", ratio);
+                FromImage();
+                canvas.getContext("2d").scale(ratio, ratio);
+            }
+
+            window.onresize = resizeCanvas;
+            resizeCanvas();
+
             var options = {
               minWidth: 0.5,
               maxWidth: 2,
@@ -16,8 +38,6 @@
               onEnd: ToImage
             };
             var signaturePad = new SignaturePad(canvas, options);
-
-            FromImage();
 
             function ToImage() {
               scope.$apply(function() {
