3759f81c40b958c6471c078989cd31219acc149f
Ankit Gupta <ankigup@adobe.com>
2016-03-15 09:51:08 +0530

    Use line for drawing

--- app/Utils/js/directives/my-canvas.directive.js
+++ app/Utils/js/directives/my-canvas.directive.js
@@ -17,55 +17,24 @@
             tool.maxDistance = 45;
 
             var path;
-
             tool.onMouseDown = function(event) {
-              path = new paper.Path();
-              path.fillColor = 'black';
-              path.fillCap = 'round'
-              path.add(event.point);
-              var dot = new paper.Path.Circle(event.point, 1.5);
-              dot.fillColor = 'black';
+                path = new paper.Path({
+                    segments: [event.point],
+                    strokeColor: 'black',
+                    strokeWidth: 3,
+                    strokeCap: 'round'
+                });
+                var dot = new paper.Path.Circle(event.point, 1.5);
+                dot.fillColor = 'black';
             }
 
             tool.onMouseDrag = function(event) {
-              var step = event.delta.divide(2);
-              var fillWidth = 1.5;
-
-              //var alpha = 1/step.length;
-              var lineWidth = fillWidth/step.length;
-              //console.log('Event.Length: ', step.length);
-              //console.log('Alpha: ', alpha);
-
-              var len = step.length;
-              //console.log('Step Before:', step.length);
-
-              var alpha = 1;
-              var velocity = alpha*((2*len)/tool.maxDistance);
-              console.log('scalar: ', (fillWidth - velocity));
-              step = step.multiply((fillWidth - velocity)/len);
-              //console.log('Step After:', step.length);
-
-              console.log('step before: ', step.angle);
-              step.angle += 90;
-              console.log('step after: ', step.angle);
-
-              //console.log('Step length: ', step.length);
-
-              console.log('\n');
-              var top = event.middlePoint.add(step);
-              var bottom = event.middlePoint.subtract(step);
-
-              path.add(top);
-              path.insert(0, bottom);
-              path.smooth({type: 'catmull-rom'});
+              path.add(event.point);
             }
 
             tool.onMouseUp = function(event) {
               path.add(event.point);
-              path.closed = true;
-              path.smooth({type: 'catmull-rom'});
-              path.rasterize();
-              path.visible = false;
+              path.simplify();
             }
         }
 
