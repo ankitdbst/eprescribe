bc5f2be874e9b7f2f28ac1d5455927565a43dee8
Mohanish Singh <mohanish.singh@gmail.com>
2016-04-28 14:57:23 +0530

    Fixed missing bs-switch

--- app/js/app.js
+++ app/js/app.js
@@ -16,6 +16,7 @@ angular.module('ERemediumWebApp', [
     'ui.select',
     'ui.calendar',
     'ngSanitize',
+    'frapontillo.bootstrap-switch',
     'ERemediumWebApp.config',
     'ERemediumWebApp.routes',
     'ERemediumWebApp.patients',
@@ -31,81 +32,81 @@ angular.module('ERemediumWebApp', [
     'ERemediumWebApp.appointments'
 ])
 
-.run(function ($rootScope, $location, $state) {
-  $rootScope.getFullName = function (inputPatientObject) {
-    if (angular.isUndefined(inputPatientObject)) {
-      return;
-    }
-    if(inputPatientObject.midlleName == undefined) {
-      inputPatientObject.midlleName = "";
-    }
-    return inputPatientObject.firstName + " " + inputPatientObject.midlleName + " " + inputPatientObject.lastName;
-  };
+        .run(function ($rootScope, $location, $state) {
+            $rootScope.getFullName = function (inputPatientObject) {
+                if (angular.isUndefined(inputPatientObject)) {
+                    return;
+                }
+                if (inputPatientObject.midlleName == undefined) {
+                    inputPatientObject.midlleName = "";
+                }
+                return inputPatientObject.firstName + " " + inputPatientObject.midlleName + " " + inputPatientObject.lastName;
+            };
 
-  $rootScope.getFullAddress = function (inputPatientObject) {
-    if (angular.isUndefined(inputPatientObject)) {
-      return;
-    }
-    return inputPatientObject.address.addressLine1 + ', ' + inputPatientObject.address.addressLine2 + ', ' + inputPatientObject.address.city + ', ' + inputPatientObject.address.state + ', ' + inputPatientObject.address.pincode;
-  };
+            $rootScope.getFullAddress = function (inputPatientObject) {
+                if (angular.isUndefined(inputPatientObject)) {
+                    return;
+                }
+                return inputPatientObject.address.addressLine1 + ', ' + inputPatientObject.address.addressLine2 + ', ' + inputPatientObject.address.city + ', ' + inputPatientObject.address.state + ', ' + inputPatientObject.address.pincode;
+            };
 
-  $rootScope.getAge = function (dateString) {
-    var today = new Date();
-    var birthDate = new Date(dateString);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age--;
-    }
-    return age;
-  }
+            $rootScope.getAge = function (dateString) {
+                var today = new Date();
+                var birthDate = new Date(dateString);
+                var age = today.getFullYear() - birthDate.getFullYear();
+                var m = today.getMonth() - birthDate.getMonth();
+                if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                    age--;
+                }
+                return age;
+            }
 
-  $rootScope.go = function (path) {
-    $location.path(path);
-  };
+            $rootScope.go = function (path) {
+                $location.path(path);
+            };
 
-  /*
-   For Auto Load a Nested View. Read More
-   http://stackoverflow.com/questions/26196906/ui-router-how-to-automatically-load-a-nested-view-within-the-parent-view-as-a-de
-   */
-  $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-    //save the previous state in a rootScope variable so that it's accessible from everywhere
-    $rootScope.previousState = fromState;
-    var aac;
-    if (aac = toState && toState.params && toState.params.autoActivateChild) {
-      $state.go(aac);
-    }
-  });
+            /*
+             For Auto Load a Nested View. Read More
+             http://stackoverflow.com/questions/26196906/ui-router-how-to-automatically-load-a-nested-view-within-the-parent-view-as-a-de
+             */
+            $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+                //save the previous state in a rootScope variable so that it's accessible from everywhere
+                $rootScope.previousState = fromState;
+                var aac;
+                if (aac = toState && toState.params && toState.params.autoActivateChild) {
+                    $state.go(aac);
+                }
+            });
 
 
-});
+        });
 
 //For correctly applying Active Class on Side Menu
 $(".sidebar-nav a").on("click", function () {
-  $(".sidebar-nav").find(".active").removeClass("active");
-  $(this).parent().addClass("active");
+    $(".sidebar-nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
 });
 
 angular.element(document).ready(function ($) {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-      $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-      $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-  });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
 
-  $('#return-to-top').click(function () {      // When arrow is clicked
-    $('body,html').animate({
-      scrollTop: 0                       // Scroll to top of body
-    }, 500);
-  });
+    $('#return-to-top').click(function () {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop: 0                       // Scroll to top of body
+        }, 500);
+    });
 
-  $('#menu-toggle-2').click(function () {      // When arrow is clicked
-    $('body,html').animate({
-      scrollTop: 0                       // Scroll to top of body
-    }, 500);
-  });
+    $('#menu-toggle-2').click(function () {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop: 0                       // Scroll to top of body
+        }, 500);
+    });
 });
 
 angular.module('ERemediumWebApp.config', []);
