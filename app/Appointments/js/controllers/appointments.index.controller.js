e2468a69c790f38d15b834701135b6f0978d53e5
Ankit Gupta <ankigup@adobe.com>
2016-05-07 07:28:42 +0530

    - Add build script in gulp for deployment. Only perform concat/uglification/minification for prod/stage environments. - Remove unnecessary bower deps

--- app/Appointments/js/controllers/appointments.index.controller.js
+++ app/Appointments/js/controllers/appointments.index.controller.js
@@ -50,68 +50,16 @@
             //             stick: true
             //           });
                 //Show a dialog to create a new Event
-                
+
                 var templateNameDialog = ngDialog.open({
                     template: 'Appointments/partials/appointments.appointment-details-dialog.html',
                     className: 'ngdialog-theme-default',
                     scope: $scope,
                     showClose: true,
                     closeByEscape: false,
-                    controller: function($scope, $stateParams, Appointments, Account) {
-                      // $scope.prescription = $scope.parent.prescription;
-                      var user = Account.getAuthenticatedAccount();
-                      $scope.selectStopTime = function(item) {
-                        console.log(item);
-                        if(item == "15 minutes") {
-                            $scope.dateTo = $scope.originalDate.add(15*60*1000).format();
-                        }
-                        else if(item == "30 minutes") {
-                            $scope.dateTo = date.add(30*60*1000).format();
-                        }
-                        else if(item == "45 minutes") {
-                            $scope.dateTo = date.add(45*60*1000).format();
-                        }
-                        else {
-                            $scope.dateTo = date.add(15*60*1000).format();
-                        }
-                      }
-                      $scope.saveAppointment = function() {
-                        $scope.eventSources[0].events.push({
-                        title: $scope.patient.firstName+' '+$scope.patient.lastName,
-                        start: $scope.dateFrom,
-                        end: $scope.dateTo,
-                        stick: true
-                      });
-                        //closeDialog
-                        ngDialog.close();
-                      }
-                      // $scope.eventSources[0].events.push({
-                      //   title: 'FCB-5555',
-                      //   start: '2016-05-01T09:30:00',
-                      //   end: '2016-05-01T10:30:00',
-                      //   stick: true
-                      // });
-                      // $scope.saveAsTemplate = UpsertPrescriptionAsTemplate;
-                      // Prescription.getTemplateById({sessionId: user.sessionId,doctorId: '101',templateId:'7575027259136053',columnsToGet:''},function(response){console.log(response);});
-                     
-                      // $scope.myPromise = Prescription.upsert(params, function (response) {
-                      //   if (_.isEqual(response.respCode, 1)) {
-                      //     $scope.closeThisDialog({
-                      //       state: 'saved',
-                      //       data: response.pid
-                      //     });
-                      //     $state.go('PrescriptionIndex.Detail', {
-                      //       prescriptionId: response.pid,
-                      //       patientId: $stateParams.patientId
-                      //     });
-                      //   } else {
-                      //     // Show Error
-                      //     console.log(response);
-                      //     }
-                      //   });
-                      }
+                    controller: 'AppointmentsDetailCtrl'
                 });
-                
+
             }
         };
         $scope.alertOnDrop = function() {
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,53 @@
 var gulp = require('gulp'),
-    gp_concat = require('gulp-concat'),
-    gp_rename = require('gulp-rename'),
-    gp_uglify = require('gulp-uglify');
+    useref = require('gulp-useref'),
+    util = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+    sourcemaps = require('gulp-sourcemaps'),
+    lazypipe = require('lazypipe'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css'),
+    del = require('del');
 
-gulp.task('compress', function() {
-  return gulp.src([
-    'app/js/app.js',
-    'app/js/app.config.js',
-    'app/**/*.module.js',
-    'app/**/*.routes.js',
-    'app/**/*.js',
-    '!app/bower_components/**/*.js',
-    '!app/components/**/*.js',
-    '!app/Utils/angular-resizable/**/*.js',
-    '!app/Utils/js/lib/*.js'])
-
-    .pipe(gp_concat('eremedium.js'))
-    .pipe(gulp.dest('app/dist'))
-    .pipe(gp_rename('eremedium.min.js'))
-    .pipe(gp_uglify())
-    .pipe(gulp.dest('app/dist'));
+// Parse html and create min.js/min.css
+gulp.task('useref', function() {
+  if (util.env.type === 'dev') return; // do nothing on dev environments
+  return gulp.src('app/**/*.html')
+    .pipe(useref({}, lazypipe().pipe(sourcemaps.init, { loadMaps: true })))
+    .pipe(sourcemaps.write('maps'))
+//    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', minifyCss()))
+    .pipe(gulp.dest('dist'));
 });
+
+gulp.task('copyImages', function() {
+  return gulp.src('app/**/*.{jpg,png,gif}')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copyFonts', function() {
+  return gulp.src('app/**/*.{ttf,otf}')
+    .pipe(gulp.dest('dist'));
+})
+
+gulp.task('copyWacomMem', function() {
+  return gulp.src('app/Utils/js/lib/*.mem', {base: 'app/Utils/js/lib'})
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['useref', 'copyWacomMem', 'copyImages', 'copyFonts']);
+
+// configure the jshint task
+gulp.task('jshint', function() {
+  return gulp.src(['app/**/*.js', '!app/bower_components/**/*.js', '!app/components/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+  gulp.watch(['app/**/*.js', '!app/bower_components/**/*.js', '!app/components/**/*.js'], ['jshint']);
+});
+
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
